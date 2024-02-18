f2d5650d1532d134a0c047dccad931b07d94709f
VitaValeeva <152518946+VitaValeeva@users.noreply.github.com>
2024-02-18 21:29:11 +0300

    js for burger & overlay plus products-slider

---
+++ javascript.js
@@ -0,0 +1,17 @@
+const burger  = document.querySelector('#burger');
+const overlay = document.querySelector('#overlay');
+const body = document.querySelector('body');
+
+let links = document.querySelectorAll('.menu__link'); [1,2,3,4,5,6]
+
+links.forEach(function(element){
+  element.addEventListener('click' , toggleMenu);
+})
+
+function toggleMenu(){
+  burger.classList.toggle('burger--active');
+  overlay.classList.toggle('overlay--active');
+  body.classList.toggle('body--active-menu');
+}
+
+burger.addEventListener('click', toggleMenu);
