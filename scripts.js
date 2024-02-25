ef339776940954db758feae03cad3c5fe0595894
VitaValeeva <152518946+VitaValeeva@users.noreply.github.com>
2024-02-26 00:54:49 +0300

    slider, accordeon, modal на проверку

--- scripts.js
+++ scripts.js
@@ -51,11 +51,11 @@ function accordionTeam () {
 
     if (target.classList.contains("team__title")) {
       const worker = target.parentNode;
-      const content = target.nextElementSiblbng;
+      const content = target.nextElementSibling;
       const contentHeight = content.firstElementChild.clientHeight;
 
       for (const iterator of workers) {
-        if (iterator != worker) {
+        if (iterator !== worker) {
           iterator.classList.remove("team__item-active");
           iterator.lastElementChild.style.height = 0;
         }
@@ -73,3 +73,109 @@ function accordionTeam () {
 }
 
 accordionTeam();
+
+//products-slider
+
+let list = document.querySelector(".products__list");
+let controls = document.querySelector(".arrow");
+let currentIndex = 0;
+
+const slider = (event) => {
+  event.preventDefault();
+  let target = event.target;
+
+  if (target.classList.contains("arrow__link")) {
+    let targetValue = target.dataset.vector;
+
+    if (targetValue === "next") {
+      if (currentIndex < list.children.length - 1) {
+        currentIndex += 1;
+        doTransition(currentIndex);
+      } else return;
+    }
+
+    if (targetValue === "prev") {
+      if (currentIndex > 0) {
+        currentIndex -= 1;
+        doTransition(currentIndex);
+      } else return;
+    }
+  }
+};
+
+function doTransition(index) {
+  list.style.transform = `translateX(-${index * 100}%)`;
+}
+
+controls.addEventListener("click", slider);
+
+//modal
+
+const validateFields = (form, fieldsArray) => {
+
+  fieldsArray.forEach((field) => {
+    field.removeClass("input-error");
+    if (field.val().trim() === "") {
+      field.addClass("input-error");
+    }
+  });
+
+  const errorFields = form.find(".input-error");
+
+  return errorFields.length === 0;
+}
+
+$(".form").submit ((e) => {
+  e.preventDefault();
+
+  const form = $(e.currentTarget);
+  const name = form.find("[name='name']");
+  const phone = form.find("[name='phone']");
+  const comment = form.find("[name='comment']");
+  const to = form.find("[name='to']");
+
+  const modal = $("#modal");
+  const content = modal.find(".modal__content");
+
+  modal.removeClass("error-modal");
+  
+  const isValid = validateFields(form, [name, phone, comment, to]);
+
+  if (isValid) {    
+    const request = $.ajax({
+      url: "https://webdev-api.loftschool.com/sendmail",
+      method: "post",
+      data: {
+        name: name.val(),
+        phone: phone.val(),
+        comment: comment.val(),
+        to: to.val(),
+      },
+
+      // error: (data) => {}
+    });
+
+    request.done((data) => {
+      content.text(data.message);          
+    });
+
+    request.fail((data) => {
+      const message = data.responseJSON.message;
+      content.text(message);
+      modal.addClass("error-modal");
+    });
+
+    request.always(() => {
+      $.fancybox.open({
+        src: "#modal",
+        type: "inline",
+      });
+    })
+  }  
+});
+
+$(".app-submit-btn").click((e) => {
+  e.preventDefault();
+
+  $.fancybox.close();
+});
